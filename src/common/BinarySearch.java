1cd3dd997528546c36e6d951ef3dd82a2d91fb2f
Sergey Kurbatov <sergey.kurbatov@avon.com>
2025-07-20 15:43:33 +0300

    count with bin search

--- src/common/BinarySearch.java
+++ src/common/BinarySearch.java
@@ -3,25 +3,56 @@ package common;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[] {0, 2, 3, 4, 5, 6}, 2));
+        System.out.println(binarySearch(new int[] {-40, -5, 0, 12, 12, 12, 12, 34, 34, 71}, 2));
+
+        System.out.println(count(new int[] {-40, -5, 0, 12, 12, 12, 12, 34, 34, 71}, 71));
     }
 
-    public static int binarySearch(int[] nums, int key) {
+    public static int binarySearch(int[] arr, int value) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = arr.length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int midVal = nums[mid];
+            int midVal = arr[mid];
 
-            if (key > midVal)
-                left = mid + 1;
-            else if (key < midVal)
-                right = mid - 1;
-            else
-                return mid;
+            if (midVal < value) left = mid + 1;
+            else if (midVal > value) right = mid - 1;
+            else return mid;
         }
 
         return -(left + 1);
+    }
+
+    public static int lowerBound(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < value) left = mid + 1;
+            else right = mid;
+        }
+
+        return left;
+    }
+
+    public static int upperBound(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > value) right = mid;
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+
+    public static int count(int[] arr, int value) {
+        return upperBound(arr, value) - lowerBound(arr, value);
     }
 }
